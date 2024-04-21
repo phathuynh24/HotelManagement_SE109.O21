b367b8fc038450fc1ee3768aad46476d5e5286a5
Phat7203 <trantienphat.mx@gmail.com>
2024-04-21 19:35:53 +0700

    feat: Add, Search and Delete Goods

---
+++ Hotel_Management/src/main/java/com/myproject/swings/GoodsTable.java
@@ -0,0 +1,48 @@
+package com.myproject.swings;
+
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+public class GoodsTable extends JTable {
+
+    public GoodsTable() {
+        setShowHorizontalLines(true);
+        setGridColor(new Color(230, 230, 230));
+        setRowHeight(40);
+        getTableHeader().setReorderingAllowed(false);
+        getTableHeader().setDefaultRenderer(new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable jtable, Object o, boolean bln, boolean bln1, int i, int i1) {
+                TableHeader header = new TableHeader(o + "");
+                header.setHorizontalAlignment(JLabel.CENTER);
+                return header;
+            }
+        });
+        setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable jtable, Object o, boolean selected, boolean bln1, int i, int i1) {
+                Component com = super.getTableCellRendererComponent(jtable, o, selected, bln1, i, i1);
+                com.setBackground(Color.WHITE);
+                setBorder(noFocusBorder);
+                 if (selected) {
+                    com.setForeground(new Color(15, 89, 140));
+                    com.setFont(com.getFont().deriveFont(java.awt.Font.BOLD));
+                } else {
+                    com.setForeground(new Color(102, 102, 102));
+                    com.setFont(com.getFont().deriveFont(java.awt.Font.PLAIN));
+                }
+                return com;
+
+            }
+        });
+    }
+
+    public void addRow(Object[] row) {
+        DefaultTableModel model = (DefaultTableModel) getModel();
+        model.addRow(row);
+    }
+}
